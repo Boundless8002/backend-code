4fe8528ea01d17a0e1117d7f1e48bf52c4b2d561
Boundless8002 <amit_rajawat8002@yahoo.in>
2024-07-08 13:07:10 +0530

    manage utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
 
-connectDB();
+// connected db return a promise
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () =>
+      console.log(` Mongodb connected at port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.log("connection failed at mongodb ", err);
+  });
